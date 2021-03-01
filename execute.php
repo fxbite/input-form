61a41efdfbbb9ef838266a25411af946dbe87ae5
fxbite <letruongthinh1703@gmail.com>
2021-03-01 09:20:31 +0700

    Edit sum for numbers

--- execute.php
+++ execute.php
@@ -9,8 +9,7 @@ Your email: <?php echo $_POST["email"];?> <br>
 
 <div>
 <h3>Your result:</h3>
-<?php echo $_POST["num1"];?> <br>
-<?php echo $_POST["num2"];?> <br>
+<?php echo $_POST["num1" + "num2"];?> <br>
 </div>
 
 </body>       
