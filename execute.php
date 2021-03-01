e9522b96220e44abcb0befa4ef93fb3d23fc8ed2
fxbite <letruongthinh1703@gmail.com>
2021-03-01 08:39:50 +0700

    Input form

---
+++ execute.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<div>
+<h3>You have submitted the following to the server:</h3>
+Your name:  <?php echo $_POST["name"];?> <br>
+Your email: <?php echo $_POST["email"];?> <br> 
+</div>
+
+<div>
+<h3>Your result:</h3>
+<?php echo $_POST["num1"];?> <br>
+<?php echo $_POST["num2"];?> <br>
+</div>
+
+</body>       
+
+</html>
