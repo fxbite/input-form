1cc4a504f2699d4c32c7d67d674c0c23293fcc8e
fxbite <letruongthinh1703@gmail.com>
2021-03-03 17:23:31 +0700

    Modify execute.php

--- execute.php
+++ execute.php
@@ -9,13 +9,13 @@ Your email: <?php echo $_POST["email"];?> <br>
 
 <?php
 $host = "ec2-34-192-58-41.compute-1.amazonaws.com";
-$dbname = "de7ic3hku7a7jf";
+$db = "de7ic3hku7a7jf";
 $port = "5432";
 $user = "msklsjfnqbydjb";
 $pass = "d0788f073d46f029bd5ef53450d31329ef669b416d7b063182cbbbef0789c74d";
 $ssl = "require";
 
-$link = pg_connect(`dbname={$dbname} host={$host} port={$port} user={$user} password={$pass} sslmode={$ssl}`);
+$link = pg_connect('dbname='.$db.' host='.$host.' port='.$port.' user='.$user.' password='.$pass.' sslmode='.$ssl);
 
 if (!$con) {
     echo "Database connection failed.";
