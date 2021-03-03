5f64a7a45a99ee8873e6b895f80705cd1570a9d2
fxbite <letruongthinh1703@gmail.com>
2021-03-03 17:14:32 +0700

    Add database connection

--- execute.php
+++ execute.php
@@ -7,19 +7,25 @@ Your name:  <?php echo $_POST["name"];?> <br>
 Your email: <?php echo $_POST["email"];?> <br> 
 </div>
 
-<div>
-<h3>Your result:</h3>
-</form>  
-<?php  
-    if(isset($_POST['submit']))  
-    {  
-        $num1 = $_POST['num1'];  
-        $num2 = $_POST['num2'];  
-        $sum =  $num1 + $num2;     
-        echo "The sum of $num1 and $num2 is: ".$sum;   
-}  
-?>  
-</div>
+<?php
+$host = "ec2-34-192-58-41.compute-1.amazonaws.com";
+$dbname = "de7ic3hku7a7jf";
+$port = "5432";
+$user = "msklsjfnqbydjb";
+$pass = "d0788f073d46f029bd5ef53450d31329ef669b416d7b063182cbbbef0789c74d";
+$ssl = "require";
+
+$link = pg_connect(`dbname={$dbname} host={$host} port={$port} user={$user} password={$pass} sslmode={$ssl}`);
+
+if (!$con) {
+    echo "Database connection failed.";
+} else {
+     echo "Database connection success.";
+}
+
+
+
+?>
 
 </body>       
 
